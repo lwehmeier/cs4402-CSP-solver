1a2659aa021e966a606f636269d211b8d67713af
Leon Wehmeier <lw96@st-andrews.ac.uk>
2018-03-31 19:50:15 +0100

    uncommitted profiling loop code

--- src/main/java/uk/ac/standrews/cs/cs4402/solver/Main.java
+++ src/main/java/uk/ac/standrews/cs/cs4402/solver/Main.java
@@ -19,17 +19,15 @@ public class Main {
 
         startTime = System.nanoTime();
 
-        for(int i=0;i<10;i++) {
-            VarNode.reset();
-            BinaryCSPReader reader = new BinaryCSPReader() ;
-            //System.out.println(reader.readBinaryCSP(inputFile)) ;
+        BinaryCSPReader reader = new BinaryCSPReader() ;
+        //System.out.println(reader.readBinaryCSP(inputFile)) ;
 
-            BinaryCSP bcsp = reader.readBinaryCSP(inputFile);
-            readTime = System.nanoTime();
+        BinaryCSP bcsp = reader.readBinaryCSP(inputFile);
+        readTime = System.nanoTime();
 
-            BinaryCSPGraph bcspg = BinaryCSPGraph.buildGraph(bcsp);
-            graphBuildTime = System.nanoTime();
-            //bcspg.draw();
+        BinaryCSPGraph bcspg = BinaryCSPGraph.buildGraph(bcsp);
+        graphBuildTime = System.nanoTime();
+        //bcspg.draw();
         /*bcspg.pruneFromVariableDomain(0, 3);
         bcspg.pruneFromVariableDomain(0, 0);
         bcspg.reviseArcs(0);
@@ -43,39 +41,33 @@ public class Main {
         //bcspg.pop();
         */
 
-            Solver solver = new ForwardSolver();
-            solver.setCSP(bcspg);
-            boolean SAT = false;
-            solverSetupTime = System.nanoTime();
-            try {
-                SAT = solver.step();
-            } catch (NoSolutionException ex) {
-                ex.printStackTrace();
-            } catch (Exception ex) {
-                ex.printStackTrace();
+        Solver solver = new ForwardSolver();
+        solver.setCSP(bcspg);
+        boolean SAT = false;
+        solverSetupTime = System.nanoTime();
+        try {
+            SAT = solver.step();
+        } catch (NoSolutionException ex) {
+            ex.printStackTrace();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        solveTime = System.nanoTime();
+        if (solver.getNumNodes() < 250)
+            solver.displaySearchTree();
+        System.out.println(SAT);
+        if (SAT) {
+            System.out.println("Assignments: ");
+            for (Integer var : solver.getAssignments().keySet()) {
+                System.out.print("Var_" + var.toString());
+                System.out.println(": " + solver.getAssignments().get(var).toString());
             }
-            solveTime = System.nanoTime();
-            if (solver.getNumNodes() < 250)
-                solver.displaySearchTree();
-            System.out.println(SAT);
-            if (SAT) {
-                System.out.println("Assignments: ");
-                for (Integer var : solver.getAssignments().keySet()) {
-                    System.out.print("Var_" + var.toString());
-                    System.out.println(": " + solver.getAssignments().get(var).toString());
-                }
-            }
-            System.out.println("Read took " + Double.toString((readTime - startTime) / 1000000) + "ms");
-            System.out.println("Graph building took " + Double.toString((graphBuildTime - readTime) / 1000000) + "ms");
-            System.out.println("Solver setup took " + Double.toString((solverSetupTime - graphBuildTime) / 1000000) + "ms");
-            System.out.println("Solving took " + Double.toString((solveTime - solverSetupTime) / 1000000) + "ms");
+        }
+        System.out.println("Read took " + Double.toString((readTime - startTime) / 1000000) + "ms");
+        System.out.println("Graph building took " + Double.toString((graphBuildTime - readTime) / 1000000) + "ms");
+        System.out.println("Solver setup took " + Double.toString((solverSetupTime - graphBuildTime) / 1000000) + "ms");
+        System.out.println("Solving took " + Double.toString((solveTime - solverSetupTime) / 1000000) + "ms");
 
-            System.out.println("SolverNodes: " + Integer.toString(solver.getNumNodes()));
-        }
-        while (true){
-            try {
-                Thread.sleep(25000);
-            }catch (InterruptedException ex){}
-        }
+        System.out.println("SolverNodes: " + Integer.toString(solver.getNumNodes()));
     }
 }
